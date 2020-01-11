260f4d3395986603d6dffa04a96575be7b23ec4a
hoyuenkim <hoyuen.kim@hanmail.net>
2020-01-11 10:02:03 +0900

    passwordHash

--- libs/passwordHash.js
+++ libs/passwordHash.js
@@ -1,23 +1,7 @@
-<<<<<<< HEAD
-const crypto = require("crypto");
-require("dotenv").config();
+const crypto = require('crypto');
+require('dotenv').config();
 const salt = process.env.SALT;
 
-module.exports = password => {
-  return crypto
-    .createHash("sha512")
-    .update(password, salt)
-    .digest("base64");
+module.exports = (password) => {
+	return crypto.createHash('sha512').update(password, salt).digest('base64');
 };
-=======
-const crypto = require("crypto");
-require("dotenv").config();
-const salt = process.env.SALT;
-
-module.exports = password => {
-  return crypto
-    .createHash("sha512")
-    .update(password, salt)
-    .digest("base64");
-};
->>>>>>> b25a21503f005cc94ce9d89228759f3b6211a0a1
