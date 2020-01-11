c74404afbc9723d7e6b10e209f6bca60d9edb312
hoyuenkim <hoyuen.kim@hanmail.net>
2020-01-11 09:56:38 +0900

    sequelize_index_change

--- models/index.js
+++ models/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config')[env];
@@ -18,24 +17,3 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
-=======
-const Sequelize = require("sequelize");
-const env = process.env.NODE_ENV || "development";
-const config = require("./config")[env];
-const db = {};
-
-module.exports = () => {
-  const sequelize = new Sequelize(config.database, config.username, config.password, config);
-
-  Object.keys(db).forEach(modelName => {
-    if (db[modelName].associate) {
-      db[modelName].associate(db);
-    }
-  });
-
-  db.sequelize = sequelize;
-  db.Sequelize = Sequelize;
-
-  module.exports = db;
-};
->>>>>>> b25a21503f005cc94ce9d89228759f3b6211a0a1
